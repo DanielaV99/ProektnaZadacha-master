84e48f8d0e72064d038b33a5ebada94a1b411a4f
DanielaV99 <vasileskadaniela@yahoo.com>
2020-12-16 15:18:28 +0100

    newcommit

--- PersonalBlog.js
+++ PersonalBlog.js
@@ -77,7 +77,7 @@ function comm()
     k1.innerHTML='<div class="row">'+
     '<div id="pa"></div>'+
     '<div class="column"><p><label>Comments:<br>'+
-        '<textarea id="ta" name="comments" rows="4" cols="30" placeholder="Enter comment here."></textarea>'+
+        '<textarea onfocusin="change(this)" onfocusout="change1(this)" id="ta" name="comments" rows="4" cols="30" placeholder="Enter comment here."></textarea>'+
         '<button type="button" onclick="comment()"><img src="comment.png" alt="Enter" height="25" width="25"></button>'+
         '<button type="button" onclick="like()"><img src="like.png" alt="Like" height="25" width="25"></button>'+
         '<button type="button" onclick="dislike()"><img src="dislike.jpg" alt="Like" height="25" width="25"></button>'+
@@ -86,7 +86,7 @@ function comm()
     
     '<div id="a"></div>'+
     '<div class="column"><p><label>Comments:<br>'+
-        '<textarea id="ta1" name="comments" rows="4" cols="30" placeholder="Enter comment here."></textarea>'+
+        '<textarea onfocusin="change(this)" onfocusout="change1(this)" id="ta1" name="comments" rows="4" cols="30" placeholder="Enter comment here."></textarea>'+
         '<button type="button" onclick="comment1()"><img src="comment.png" alt="Enter" height="25" width="25"></button>'+
         '<button type="button" onclick="like1()"><img src="like.png" alt="Like" height="25" width="25"></button>'+
         '<button type="button" onclick="dislike1()"><img src="dislike.jpg" alt="Like" height="25" width="25"></button>'+
@@ -95,7 +95,7 @@ function comm()
     
     '<div id="l"></div>'+
     '<div class="column"><p><label>Comments:<br>'+
-        '<textarea id="ta2" name="comments" rows="4" cols="30" placeholder="Enter comment here."></textarea>'+
+        '<textarea onfocusin="change(this)" onfocusout="change1(this)" id="ta2" name="comments" rows="4" cols="30" placeholder="Enter comment here."></textarea>'+
         '<button type="button" onclick="comment2()"><img src="comment.png" alt="Enter" height="25" width="25"></button>'+
         '<button type="button" onclick="like2()"><img src="like.png" alt="Like" height="25" width="25"></button>'+
         '<button type="button" onclick="dislike2()"><img src="dislike.jpg" alt="Like" height="25" width="25"></button>'+
@@ -112,7 +112,7 @@ function comm1()
     k2.innerHTML='<div class="row">'+
     '<div id="t"></div>'+
     '<div class="column"><p><label>Comments:<br>'+
-        '<textarea id="ta11" name="comments" rows="4" cols="30" placeholder="Enter comment here."></textarea>'+
+        '<textarea onfocusin="change(this)" onfocusout="change1(this)" id="ta11" name="comments" rows="4" cols="30" placeholder="Enter comment here."></textarea>'+
         '<button type="button" onclick="comment11()"><img src="comment.png" alt="Enter" height="25" width="25"></button>'+
         '<button type="button" onclick="like11()"><img src="like.png" alt="Like" height="25" width="25"></button>'+
         '<button type="button" onclick="dislike11()"><img src="dislike.jpg" alt="Like" height="25" width="25"></button>'+
@@ -121,7 +121,7 @@ function comm1()
     
     '<div id="z"></div>'+
     '<div class="column"><p><label>Comments:<br>'+
-        '<textarea id="ta12" name="comments" rows="4" cols="30" placeholder="Enter comment here."></textarea>'+
+        '<textarea onfocusin="change(this)" onfocusout="change1(this)" id="ta12" name="comments" rows="4" cols="30" placeholder="Enter comment here."></textarea>'+
         '<button type="button" onclick="comment12()"><img src="comment.png" alt="Enter" height="25" width="25"></button>'+
         '<button type="button" onclick="like12()"><img src="like.png" alt="Like" height="25" width="25"></button>'+
         '<button type="button" onclick="dislike12()"><img src="dislike.jpg" alt="Like" height="25" width="25"></button>'+
@@ -130,7 +130,7 @@ function comm1()
     
     '<div id="s"></div>'+
     '<div class="column"><p><label>Comments:<br>'+
-        '<textarea id="ta13" name="comments" rows="4" cols="30" placeholder="Enter comment here."></textarea>'+
+        '<textarea onfocusin="change(this)" onfocusout="change1(this)" id="ta13" name="comments" rows="4" cols="30" placeholder="Enter comment here."></textarea>'+
         '<button type="button" onclick="comment13()"><img src="comment.png" alt="Enter" height="25" width="25"></button>'+
         '<button type="button" onclick="like13()"><img src="like.png" alt="Like" height="25" width="25"></button>'+
         '<button type="button" onclick="dislike13()"><img src="dislike.jpg" alt="Like" height="25" width="25"></button>'+
@@ -138,6 +138,16 @@ function comm1()
     '</div>'+
 '</div>'
 
+}
+
+function change(x)
+{
+    x.style.background="gray";
+}
+
+function change1(x)
+{
+    x.style.background="white";
 }
 
 function blog()
@@ -156,8 +166,8 @@ function recenzija()
 
 function comment()
 {
-    var sl=document.getElementById("ta").value;
-    document.getElementById("pa").innerHTML+="<b>"+sl+"</b>"+"\t"+date.getHours()+":"+date.getMinutes()+"\t"+date.getDate()+"/"+dato()+"/"+date.getFullYear()+"<br>";
+  var sl=document.getElementById("ta").value;
+  document.getElementById("pa").innerHTML+="<b>"+sl+"</b>"+"\t"+date.getHours()+":"+date.getMinutes()+"\t"+date.getDate()+"/"+dato()+"/"+date.getFullYear()+"<br>";
 }
 function comment1()
 {
@@ -186,96 +196,237 @@ function comment13()
     document.getElementById("s").innerHTML+="<b>"+sl+"</b>"+"\t"+date.getHours()+":"+date.getMinutes()+"\t"+date.getDate()+"/"+dato()+"/"+date.getFullYear()+"<br>";
 }
 
-var klikovi=0;
 function like()
 {
-    klikovi+=1;
-    document.getElementById("paris").innerHTML="<b>"+klikovi+"\t likes</b>";
+        if (typeof(Storage) !== "undefined") {
+          if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount)+1;
+          } else {
+            localStorage.clickcount = 1;
+          }
+          document.getElementById("paris").innerHTML = "<b>" + localStorage.clickcount + "\t likes</b>";
+        } else {
+          document.getElementById("paris").innerHTML = "Sorry, your browser does not support web storage...";
+        }
+   
 }
 
-var klikovi1=0;
 function like1()
 {
-    klikovi1+=1;
-    document.getElementById("amsterdam").innerHTML="<b>"+klikovi1+"\t likes</b>";
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount1) {
+          localStorage.clickcount1 = Number(localStorage.clickcount1)+1;
+        } else {
+          localStorage.clickcount1 = 1;
+        }
+        document.getElementById("amsterdam").innerHTML = "<b>" + localStorage.clickcount1 + "\t likes</b>";
+      } else {
+        document.getElementById("amsterdam").innerHTML = "Sorry, your browser does not support web storage...";
+      }
 }
 
-var klikovi2=0;
+
 function like2()
 {
-    klikovi2+=1;
-    document.getElementById("london").innerHTML="<b>"+klikovi2+"\t likes</b>";
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount2) {
+          localStorage.clickcount2 = Number(localStorage.clickcount2)+1;
+        } else {
+          localStorage.clickcount2 = 1;
+        }
+        document.getElementById("london").innerHTML = "<b>" + localStorage.clickcount2 + "\t likes</b>";
+      } else {
+        document.getElementById("london").innerHTML = "Sorry, your browser does not support web storage...";
+      }
+
 }
-var klikovi3=0;
+
 function like11()
 {
-    klikovi3+=1;
-    document.getElementById("tigar").innerHTML="<b>"+klikovi3+"\t likes</b>";
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount3) {
+          localStorage.clickcount3 = Number(localStorage.clickcount3)+1;
+        } else {
+          localStorage.clickcount3 = 1;
+        }
+        document.getElementById("tigar").innerHTML = "<b>" + localStorage.clickcount3 + "\t likes</b>";
+      } else {
+        document.getElementById("tigar").innerHTML = "Sorry, your browser does not support web storage...";
+      }
 }
-var klikovi4=0;
+
 function like12()
 {
-    klikovi4+=1;
-    document.getElementById("zirafa").innerHTML="<b>"+klikovi4+"\t likes</b>";
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount4) {
+          localStorage.clickcount4 = Number(localStorage.clickcount4)+1;
+        } else {
+          localStorage.clickcount4 = 1;
+        }
+        document.getElementById("zirafa").innerHTML = "<b>" + localStorage.clickcount4 + "\t likes</b>";
+      } else {
+        document.getElementById("zirafa").innerHTML = "Sorry, your browser does not support web storage...";
+      }
 }
-var klikovi5=0;
+
 function like13()
 {
-    klikovi5+=1;
-    document.getElementById("slon").innerHTML="<b>"+klikovi5+"\t likes</b>";
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount5) {
+          localStorage.clickcount5 = Number(localStorage.clickcount5)+1;
+        } else {
+          localStorage.clickcount5 = 1;
+        }
+        document.getElementById("slon").innerHTML = "<b>" + localStorage.clickcount5 + "\t likes</b>";
+      } else {
+        document.getElementById("slon").innerHTML = "Sorry, your browser does not support web storage...";
+      }
 }
 
 
-var dis=0;
+
 function dislike()
 {
-    dis+=1;
-    document.getElementById("dparis").innerHTML="<b>"+dis+"\t dislikes</b>";
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount6) {
+          localStorage.clickcount6 = Number(localStorage.clickcount6)+1;
+        } else {
+          localStorage.clickcount6 = 1;
+        }
+        document.getElementById("dparis").innerHTML = "<b>" + localStorage.clickcount6 + "\t dislikes</b>";
+      } else {
+        document.getElementById("dparis").innerHTML = "Sorry, your browser does not support web storage...";
+      }
+
 }
 
-var dis1=0;
 function dislike1()
 {
-    dis1+=1;
-    document.getElementById("damsterdam").innerHTML="<b>"+dis1+"\t dislikes</b>";
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount7) {
+          localStorage.clickcount7 = Number(localStorage.clickcount7)+1;
+        } else {
+          localStorage.clickcount7 = 1;
+        }
+        document.getElementById("damsterdam").innerHTML = "<b>" + localStorage.clickcount7 + "\t dislikes</b>";
+      } else {
+        document.getElementById("damsterdam").innerHTML = "Sorry, your browser does not support web storage...";
+      }
+
+
 }
 
-var dis2=0;
+
 function dislike2()
 {
-    dis2+=1;
-    document.getElementById("dlondon").innerHTML="<b>"+dis2+"\t dislikes</b>";
-}
-var dis3=0;
-function dislike11()
-{
-    dis3+=1;
-    document.getElementById("dtigar").innerHTML="<b>"+dis3+"\t dislikes</b>";
-}
-var dis4=0;
-function dislike12()
-{
-    dis4+=1;
-    document.getElementById("dzirafa").innerHTML="<b>"+dis4+"\t dislikes</b>";
-}
-var dis5=0;
-function dislike13()
-{
-    dis5+=1;
-    document.getElementById("dslon").innerHTML="<b>"+dis5+"\t dislikes</b>";
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount8) {
+          localStorage.clickcount8 = Number(localStorage.clickcount8)+1;
+        } else {
+          localStorage.clickcount8 = 1;
+        }
+        document.getElementById("dlondon").innerHTML = "<b>" + localStorage.clickcount8 + "\t dislikes</b>";
+      } else {
+        document.getElementById("dlondon").innerHTML = "Sorry, your browser does not support web storage...";
+      }
+
 }
 
-function swap()
+function dislike11()
 {
-   var opcii=["kniga","knigazadna"];
-   var index=Math.floor(Math.random()*2);
-   iconImg=document.getElementById("image")
-   iconImg.setAttribute( "src", opcii[index] + ".jpg" );
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount9) {
+          localStorage.clickcount9 = Number(localStorage.clickcount9)+1;
+        } else {
+          localStorage.clickcount9 = 1;
+        }
+        document.getElementById("dtigar").innerHTML = "<b>" + localStorage.clickcount9 + "\t dislikes</b>";
+      } else {
+        document.getElementById("dtigar").innerHTML = "Sorry, your browser does not support web storage...";
+      }
+}
+
+function dislike12()
+{
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount10) {
+          localStorage.clickcount10 = Number(localStorage.clickcount10)+1;
+        } else {
+          localStorage.clickcount10 = 1;
+        }
+        document.getElementById("dzirafa").innerHTML = "<b>" + localStorage.clickcount10 + "\t dislikes</b>";
+      } else {
+        document.getElementById("dzirafa").innerHTML = "Sorry, your browser does not support web storage...";
+      }
+}
+
+function dislike13()
+{
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount11) {
+          localStorage.clickcount11 = Number(localStorage.clickcount11)+1;
+        } else {
+          localStorage.clickcount11 = 1;
+        }
+        document.getElementById("dslon").innerHTML = "<b>" + localStorage.clickcount11 + "\t dislikes</b>";
+      } else {
+        document.getElementById("dslon").innerHTML = "Sorry, your browser does not support web storage...";
+      }
+}
+
+function swap(id,primary,secondary)
+{
+    src=document.getElementById(id).src;
+    if (src.match(primary)) {
+      document.getElementById(id).src=secondary;
+    } else {
+      document.getElementById(id).src=primary;
+    }
 }
 
 function anket()
 {
     window.alert("Успешно направена анкета!");
+}
+
+function belavoda()
+{
+  window.open("https://www.facebook.com/restoranbelavoda","Restoran Bela Voda");
+}
+
+function mari()
+{
+  window.open("https://www.facebook.com/mariloungeandbar","Lounge and Bar Mari");
+}
+
+function gardenia()
+{
+  window.open("https://www.facebook.com/HotelGardeniaMK","Hotel Gardenia");
+}
+
+function serija1()
+{
+  window.open("https://mk.wikipedia.org/wiki/%D0%9F%D1%80%D0%B8%D1%98%D0%B0%D1%82%D0%B5%D0%BB%D0%B8_(%D0%A2%D0%92_%D1%81%D0%B5%D1%80%D0%B8%D1%98%D0%B0)","Friends");
+}
+
+function serija2()
+{
+  window.open("https://en.wikipedia.org/wiki/Money_Heist","Money Heist");
+}
+
+function serija3()
+{
+  window.open("https://en.wikipedia.org/wiki/Prison_Break","Prison Break");
+}
+
+function fb()
+{
+  window.open("https://www.facebook.com/dvasileska.14","Facebook");
+}
+
+function insta()
+{
+  window.open("https://www.instagram.com/vasileska_daniela/?hl=en","Instagram");
 }
 
 
